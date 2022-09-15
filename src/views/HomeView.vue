d511b140e9e9815043694f223853af4e215a3745
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2022-09-15 13:43:48 +0100

    adding axios, JSON server and displaying data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <JobCard v-for="job in jobs" :key="job.id" :job="job" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import JobCard from "@/components/JobCard.vue";
+import api from "@/api";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    JobCard,
+  },
+  data() {
+    return {
+      jobs: [],
+      isLoading: true,
+    };
+  },
+  created() {
+    api.getJobs().then((response) => {
+      this.jobs = response.data;
+      this.isLoading = false;
+    });
   },
 };
 </script>
